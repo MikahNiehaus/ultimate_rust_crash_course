1a2246ca8c73dc83abe8bc49f1de58e8354f6de0
Mikah Niehaus <Graywolf.niehaus@live.com>
2020-12-10 13:16:14 -0600

    did exercise/h-closures-threads

--- exercise/g-collections-enums/src/main.rs
+++ exercise/g-collections-enums/src/main.rs
@@ -1,15 +1,5 @@
 // Silence some warnings that could distract from the exercise
 #![allow(unused_variables, unused_mut, dead_code)]
-
-// Someone is shooting arrows at a target.  We need to classify the shots.
-//
-// 1a. Create an enum called `Shot` with variants:
-// - `Bullseye`
-// - `Hit`, containing the distance from the center (an f64)
-// - `Miss`
-//
-// You will need to complete 1b as well before you will be able to run this program successfully.
-
 enum Shot {
     Bullseye,
     Hit(f64),
@@ -19,11 +9,7 @@ enum Shot {
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
-        // 1b. Implement this method to convert a Shot into points
-        // - return 5 points if `self` is a `Shot::Bullseye`
-        // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
-        // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
-        // - return 0 points if `self` is a Miss
+      
         match self {
             Shot::Bullseye => 5,
             Shot::Hit(x) if x < 3.0 => 2,
@@ -39,14 +25,7 @@ fn main() {
     let arrow_coords: Vec<Coord> = get_arrow_coords(5);
     let mut shots: Vec<Shot> = Vec::new();
 
-    // 2. For each coord in arrow_coords:
-    //
-    //   A. Call `coord.print_description()`
-    //   B. Append the correct variant of `Shot` to the `shots` vector depending on the value of
-    //   `coord.distance_from_center()`
-    //      - Less than 1.0 -- `Shot::Bullseye`
-    //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
-    //      - Greater than 5.0 -- `Shot::Miss`
+   
     for coord in arrow_coords{
         coord.print_description();
         let shot = match coord.distance_from_center(){
